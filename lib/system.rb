b627c27f39e6608331aa1b7b215492d4ba219bf1
KKennedyCodes <holmstedtk@gmail.com>
2019-09-05 15:45:23 -0700

    Hotel System can Make and Store a Reservation

--- lib/system.rb
+++ lib/system.rb
@@ -4,23 +4,42 @@ require_relative 'reservation_date.rb'
 
 module Hotel
   class System
-    attr_reader :rooms, :reservations
+    attr_reader :rooms, :reservations, :start_date, :end_date
     
-    def initialize# :room_num, :start_date, :end_date, :rate
+    def initialize #:room_num = nil, :start_date = nil, :end_date = nil, :rate = nil
       @rooms = Hotel::Room.all
       @reservations = Array.new
+      @new_reservation = nil #temporary varialbe for booking
     end
-    
-    # def pre-reservation_validation
-    #   #search through current reservations - find available room
-    # end
     
     # def add_reservation(new_reservation)
     #   reservations << new_reservation
     # end
+    
     # def room_list
     #   return Hotel::Room.all
     # end
+    def create_reservation(start_date, end_date)
+      @new_reservation = Hotel::Reservation.new(start_date: start_date, end_date: end_date)
+    end
+    
+    def add_reservation(new_reservation)
+      @reservations << new_reservation
+    end
+    
+    # def reservation_validation
+    #   search through current reservations - find available room
+    # end
+    
+    # def reservation_check_all
+    #   if @reservations.length = 0
+    #     raise ArgumentError.new("No Reservations in the System")
+    #   end
+    
+    #   i = 0
+    #   while i < @reservations.length
+    #   end
+    
     
   end
 end
--- test/system_test.rb
+++ test/system_test.rb
@@ -21,14 +21,31 @@ describe "System" do
       hotel_system.reservations.must_be_kind_of Array
     end
     
-    # describe "Methods" do
-    #   describe "Room Class Access" do
-    #     it "Can Access a List of Rooms in the Hotel" do
-    #       room_list = Hotel::Room.all
-    #       room_list.must_be_kind_of Array
-    #       #Other Tests on Hotel::Room can be found in room_test.rb
-    #     end
-    #   end
-    # end
+    describe "Methods" do
+      before do
+        @hotel_system = Hotel::System.new
+      end
+      
+      it "Can Create a New Reservation" do
+        reservation = @hotel_system.create_reservation("2014-02-04", "2014-02-05")
+        reservation.must_be_kind_of Hotel::Reservation
+      end
+      
+      it "Can Add a New Reservation to the Existing List" do
+        reservation = @hotel_system.create_reservation("2014-02-04", "2014-02-05")
+        5.times do
+          @hotel_system.add_reservation(reservation)
+        end
+        @hotel_system.reservations.length.must_equal 5
+      end
+      
+      # describe "Room Class Access" do
+      #   it "Can Access a List of Rooms in the Hotel" do
+      #     room_list = Hotel::Room.all
+      #     room_list.must_be_kind_of Array
+      #     #Other Tests on Hotel::Room can be found in room_test.rb
+      #   end
+      # end
+    end
   end
 end
